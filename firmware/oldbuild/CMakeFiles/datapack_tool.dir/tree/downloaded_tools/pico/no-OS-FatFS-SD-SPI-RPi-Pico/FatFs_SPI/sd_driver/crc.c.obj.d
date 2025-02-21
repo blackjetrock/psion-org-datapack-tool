338656e0e6ebe0fe780ec146e91da5ced797e9f3
blackrockjet <menadue@gmail.com>
2025-02-21 19:18:02 +0000

    Updates

---
+++ firmware/oldbuild/CMakeFiles/datapack_tool.dir/tree/downloaded_tools/pico/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/datapack_tool.dir/tree/downloaded_tools/pico/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj: \
+ /tree/downloaded_tools/pico/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c \
+ /tree/downloaded_tools/pico/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h
